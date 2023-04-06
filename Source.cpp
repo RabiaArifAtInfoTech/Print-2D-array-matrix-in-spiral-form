2a86b8f4d40fe207945e1ed92f17603b6069598b
RabiaArifAtInfoTech <125559764+RabiaArifAtInfoTech@users.noreply.github.com>
2023-04-06 09:44:57 +0500

    Printing 2d matrix/array in spiral form using recursive method
    Now perfectly displaying 2D array of any size without making function restricted to one size of array

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int k = 0;
 
-void print2DArrayInSpiralForm(int arr[4][4], int rowI, int colI, int rowF, int colF) {
+void print2DArrayInSpiralForm(int **arr, int rowI, int colI, int rowF, int colF) {
 
     int element;
 
@@ -14,50 +14,54 @@ void print2DArrayInSpiralForm(int arr[4][4], int rowI, int colI, int rowF, int c
 
     int i = rowI;
 
-    cout << "1st row\n\n" << endl;
+    //cout << "1st row\n\n" << endl;
 
     for (int j = colI; j < colF; j++)
     {
-        cout << i << " " << j << endl;
+       // cout << i << " " << j << endl;
         element = arr[i][j];
-        cout << "arrtemp[" << k << "]: " << element << endl;
+        cout << element << " ";
+      //  cout << "arrtemp[" << k << "]: " << element << endl;
         k++;
     }
 
-    cout << "\n\nLast col\n\n" << endl;
+   // cout << "\n\nLast col\n\n" << endl;
 
     int l = colF - 1;
     for (int m = rowI + 1; m < rowF; m++)
     {
-        cout << m << " " << l << endl;
+      //  cout << m << " " << l << endl;
         element = arr[m][l];
-        cout << "arrtemp[" << k << "]: " << element << endl;
+        cout << element << " ";
+        //  cout << "arrtemp[" << k << "]: " << element << endl;
         k++;
     }
   
 
    
-    cout << "\n\nlast row\n\n" << endl;
+  //  cout << "\n\nlast row\n\n" << endl;
 
 
     int z = rowF - 1;
     for (int o = colF - 2; o >= colI; o--)
     {
-        cout << z << " " << o << endl;
+       // cout << z << " " << o << endl;
         element = arr[z][o];
-        cout << "arrtemp[" << k << "]: " << element << endl;
+        cout << element << " ";
+        //  cout << "arrtemp[" << k << "]: " << element << endl;
         k++;
     }
    
 
-    cout << "\n\nFirst col\n\n" << endl;
+  //  cout << "\n\nFirst col\n\n" << endl;
 
     int y = colI;
     for (int u = rowF - 2; u > rowI; u--)
     {
-        cout << u << " " << y << endl;
+       // cout << u << " " << y << endl;
         element = arr[u][y];
-        cout << "arrtemp[" << k << "]: " << element << endl;
+        cout << element <<" ";
+        //  cout << "arrtemp[" << k << "]: " << element << endl;
         k++;
     }
 
@@ -80,15 +84,37 @@ int main()
  //       //}
  //     
  //   }
+    int** arr = new int* [4];
+    for (int i = 0; i < 4; i++) {
+        arr[i] = new int[4];
+    }
 
-
-    int arr[4][4] = { { 1, 2, 3, 4 },
-                    { 5, 6, 7, 8 },
-                    { 9, 10, 11, 12 },
-                    { 13, 14, 15, 16 } };
-
+    // Initialize the array
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            arr[i][j] = i * 4 + j + 1;
+        }
+    }
 
     print2DArrayInSpiralForm(arr, 0, 0, 4, 4);
+
+    cout << "\n\n\n";
+
+    int** arr1 = new int* [5];
+    for (int i = 0; i < 5; i++) {
+        arr1[i] = new int[5];
+    }
+
+    // Initialize the array
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            arr1[i][j] = i * 5 + j + 1;
+        }
+    }
+
+    print2DArrayInSpiralForm(arr1, 0, 0, 5, 5);
+
+
   
     cout << "\n\n\n";
     return 0;
